a81c3ee60bddd51194fe16bb2503bff45511e9aa
Musap Kahraman <musap86@gmail.com>
2018-05-12 12:02:41 +0300

    Logic for landscape and tablet layouts implemented.

--- app/src/main/java/com/example/baking/ui/RecipeDetailActivity.java
+++ app/src/main/java/com/example/baking/ui/RecipeDetailActivity.java
@@ -13,7 +13,9 @@ import com.example.baking.data.RecipesViewModel;
 import com.example.baking.data.database.entity.Ingredient;
 import com.example.baking.data.database.entity.Step;
 import com.example.baking.ui.adapters.RecipeStepClickListener;
+import com.example.baking.ui.fragments.MediaPlayerFragment;
 import com.example.baking.ui.fragments.RecipeDetailFragment;
+import com.example.baking.ui.fragments.StepInstructionFragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,10 +23,15 @@ import java.util.List;
 public class RecipeDetailActivity extends AppCompatActivity implements RecipeStepClickListener {
     private int mStepCount;
     private String mRecipeName;
+    private boolean mTwoPane;
+    private RecipesViewModel mViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
+
+        mTwoPane = findViewById(R.id.linear_layout_step_details) != null;
 
         String recipeName = getIntent().getStringExtra("name");
         if (recipeName != null) {
@@ -35,8 +42,8 @@ public class RecipeDetailActivity extends AppCompatActivity implements RecipeSte
         final RecipeDetailFragment recipeDetailFragment = new RecipeDetailFragment();
         final FragmentManager fragmentManager = getSupportFragmentManager();
         final int id = getIntent().getIntExtra("id", 1);
-
-        final RecipesViewModel viewModel = ViewModelProviders.of(this).get(RecipesViewModel.class);
+        mViewModel = ViewModelProviders.of(this).get(RecipesViewModel.class);
+        final RecipesViewModel viewModel = mViewModel;
         viewModel.getSteps(id).observe(this, new Observer<List<Step>>() {
             @Override
             public void onChanged(@Nullable final List<Step> steps) {
@@ -49,18 +56,50 @@ public class RecipeDetailActivity extends AppCompatActivity implements RecipeSte
                         bundle.putParcelableArrayList("ingredients", (ArrayList<Ingredient>) ingredients);
                         recipeDetailFragment.setArguments(bundle);
                         fragmentManager.beginTransaction().replace(R.id.recipe_steps_container, recipeDetailFragment).commit();
+
+                        if (mTwoPane) {
+                            int defaultStepId = id * 100 + 1;
+                            populateStepDetails(defaultStepId);
+                        }
                     }
                 });
             }
         });
 
     }
+
     @Override
     public void onRecipeStepClick(int databaseGeneratedStepId) {
-        Intent intent = new Intent(RecipeDetailActivity.this, RecipeStepDetailsActivity.class);
-        intent.putExtra("id", databaseGeneratedStepId);
-        intent.putExtra("name", mRecipeName);
-        intent.putExtra("stepCount", mStepCount);
-        startActivity(intent);
+        if (mTwoPane) {
+            populateStepDetails(databaseGeneratedStepId);
+        } else {
+            Intent intent = new Intent(RecipeDetailActivity.this, RecipeStepDetailsActivity.class);
+            intent.putExtra("id", databaseGeneratedStepId);
+            intent.putExtra("name", mRecipeName);
+            intent.putExtra("stepCount", mStepCount);
+            startActivity(intent);
+        }
+    }
+
+    private void populateStepDetails(final int id) {
+        final MediaPlayerFragment mediaPlayerFragment = new MediaPlayerFragment();
+        final StepInstructionFragment stepInstructionFragment = new StepInstructionFragment();
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        final int stepCount = mStepCount;
+        mViewModel.getStep(id).observe(this, new Observer<Step>() {
+            @Override
+            public void onChanged(@Nullable Step step) {
+                Bundle bundle = new Bundle();
+                bundle.putString("video", step.videoURL);
+                bundle.putString("thumbnail", step.thumbnailURL);
+                bundle.putString("description", step.description);
+                bundle.putInt("stepCount", stepCount);
+                bundle.putInt("id", id);
+                mediaPlayerFragment.setArguments(bundle);
+                stepInstructionFragment.setArguments(bundle);
+                fragmentManager.beginTransaction().replace(R.id.media_player_container, mediaPlayerFragment).commit();
+                fragmentManager.beginTransaction().replace(R.id.step_instruction_container, stepInstructionFragment).commit();
+            }
+        });
     }
 }
